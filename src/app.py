ff456100bd1947ee15671a385a7fb898d76c4e88
Gagan Thapar <gaganthapar@gagans-mbp.lan>
2024-09-15 07:08:29 -0400

    new code

--- src/app.py
+++ src/app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template
-
 import os
-
 
 app = Flask(__name__)
 
@@ -11,5 +9,7 @@ def home():
 
 if __name__ == '__main__':
     print("Current working directory:", os.getcwd())
+    print("Files in the current directory:", os.listdir())
+    print("Files in 'templates' directory:", os.listdir('templates'))
 
     app.run(debug=True)
