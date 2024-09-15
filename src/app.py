4b8a9fb5f4c4b1782be16201d98e038bbc80ed13
Gagan Thapar <gaganthapar@gagans-mbp.lan>
2024-09-15 07:01:12 -0400

    new code

--- src/app.py
+++ src/app.py
@@ -1,49 +1,10 @@
-from flask import Flask, jsonify, render_template
-from src.models import db, NewsArticle
-from src.news_fetcher import fetch_and_process_news
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-uri = os.getenv("DATABASE_URL")
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-
-with app.app_context():
-    db.create_all()
-
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-
-@app.route('/fetch_news', methods=['GET'])
-def fetch_news():
-    try:
-        articles = fetch_and_process_news()
-
-        for article in articles:
-            if not NewsArticle.query.filter_by(url=article["url"]).first():
-                news_article = NewsArticle(
-                    title=article["title"],
-                    url=article["url"],
-                    source=article["source"],
-                    sentiment=article["sentiment"],
-                    published_at=article["published_at"]
-                )
-                db.session.add(news_article)
-
-        db.session.commit()
-
-        return jsonify({"message": "News articles fetched and saved successfully", "data": articles}), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
 
 if __name__ == '__main__':
     app.run(debug=True)
